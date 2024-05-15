26ae15ce7355709b6649578e5c8eafc6d336e854
Liza <lisalisalisa57@mail.ru>
2024-05-15 20:19:38 +0300

    Initial

---
+++ script.js
@@ -0,0 +1,57 @@
+const start = document.querySelector('.start');
+const screens = document.querySelectorAll('.screen');
+
+start.addEventListener('click', event => {
+    screens[0].classList.add('up');
+})
+
+const images = document.querySelectorAll('.slider-img');
+const controlls = document.querySelectorAll('.controlls');
+let imageIndex = 0;
+
+function show(index) {
+    images[imageIndex].classList.remove('active');
+    images[index].classList.add('active');
+    imageIndex = index;
+}
+
+controlls.forEach((e) => {
+    e.addEventListener('click', () => {
+        if (event.target.classList.contains('prev')) {
+            let index = imageIndex - 1;
+
+            if (index < 0) {
+                index = images.length - 1;
+            }
+
+            show(index);
+        } else if (event.target.classList.contains('next')) {
+            let index = imageIndex + 1;
+            if (index >= images.length) {
+                index = 0;
+            }
+            show(index);
+        }
+    })
+})
+
+document.addEventListener('keydown', 
+event => {
+    if (event.key === 'ArrowDown' || event.key === 'ArrowLeft') {
+        let index = imageIndex + 1;
+  if (index >= images.length) {
+    index = 0;
+  }
+  show(index);
+    } else if (event.key === 'ArrowUp' || event.key === 'ArrowRight') {
+        let index = imageIndex - 1;
+
+  if (index < 0) {
+    index = images.length - 1;
+  }
+
+  show(index);
+    }
+})
+
+show(imageIndex);
